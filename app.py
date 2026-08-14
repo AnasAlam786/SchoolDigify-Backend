ec330acd26c10b381ae2191397a4e83f86b25a8f
Anas Alam <anasalam702@gmail.com>
2026-08-15 01:03:19 +0530

    added 0.0.0 instead of local host

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = create_app()
 
 if __name__ == "__main__":
     # app.run(port=2000, debug=True)
-    port = int(os.environ.get("PORT", 5000))  # fallback for local testing
-    app.run(host="localhost", port=port, debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
     #app.run(debug=True)
       
     # On Windows:
