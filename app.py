711efde8984536fbf3bef394ac8179cae70bad45
Anas Alam <anasalam702@gmail.com>
2026-08-24 09:01:20 +0530

    Fixing some errors

--- app.py
+++ app.py
@@ -4,10 +4,5 @@ from src import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    # app.run(port=2000, debug=True)
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
-    #app.run(debug=True)
-      
-    # On Windows:
-    # .venv\Scripts\activate
